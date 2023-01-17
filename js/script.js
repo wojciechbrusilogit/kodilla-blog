b3a5bbb522337f92fe02b4ba513130d72742bfd6
Wojciech Brusilo <brusilo124@gmail.com>
2023-01-17 14:52:03 +0100

    add .active to clickedElement

--- js/script.js
+++ js/script.js
@@ -3,17 +3,18 @@
 function titleClickHandler(event){
   console.log('Link was clicked!');
   console.log (event)
-
-  /* remove class 'active' from all article links  */
+  console.log('clickedElement (with plus): ' + clickedElement);
+  /* [DONE] remove class 'active' from all article links  */
   const activeLinks = document.querySelectorAll('.titles a.active');
 
   for(let activeLink of activeLinks){
     activeLink.classList.remove('active');
   }
-  /* add class 'active' to the clicked link */
+  /* [IN PROGRESS]  add class 'active' to the clicked link */
+  console.log('clickedElement:', clickedElement);
+  clickedElement.classList.add('active')
 
-
-  /* remove class 'active' from all articles */
+  /* [DONE] remove class 'active' from all articles */
   const activeArticles = document.querySelectorAll('.titles a.active');
 
 for(let activeArticle of activeArticles){
@@ -28,6 +29,7 @@ for(let activeArticle of activeArticles){
 }
 
 const links = document.querySelectorAll('.titles a');
+const clickedElement = this;
 
 for(let link of links){
   link.addEventListener('click', titleClickHandler);
